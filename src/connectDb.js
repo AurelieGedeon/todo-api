14988dc65056de69a22ac83597d2bb94789e2612
Aurelie Gedeon <agedeon540@gmail.com>
2022-02-17 11:24:52 -0500

    created connection to the database

---
+++ src/connectDb.js
@@ -0,0 +1,15 @@
+const { initializeApp, cert, getApps } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
+const serviceAccount = require("../credentials.json");
+
+exports.connectDb = () => {
+  if (!getApps().length) {
+    initializeApp({
+      credential: cert(serviceAccount),
+    });
+  }
+  return getFirestore();
+};
+
+//We can use this file for other projects!
--- src/tasks.js
+++ src/tasks.js
@@ -1,9 +1,23 @@
+const { response } = require("express");
+const { connectDb } = require("./connectDb");
+
 // exports.createTask = (req, res) => {
 
 // }
 
 exports.getTasks = (req, res) => {
-  res.send("Get Tasks is working.");
+  const db = connectDb();
+  db.collection("tasks")
+    .get()
+    .then((snapshot) => {
+      const taskList = snapshot.docs.map((doc) => {
+        let task = dot.data();
+        task.id = doc.id;
+        return task;
+      });
+      res.send(taskList);
+    })
+    .catch((err) => res.status(500).send(err));
 };
 
 // exports.updateTask = (req, res) => {
